27986ef2286bfc0a0838d11d0ef40a2ee3d48aca
Warren Powers <wpp@thermoanalytics.com>
2025-10-06 11:29:56 -0400

    Run clang-format on these files in preparation to modify the source.
    Detailed list of changes:
      ../Src/LinearSolvers/MLMG/AMReX_MLCellABecLap.H
      ../Src/LinearSolvers/MLMG/AMReX_MLCellABecLap_3D_K.H
      ../Src/LinearSolvers/MLMG/AMReX_MLPoisson.H
      ../Src/LinearSolvers/MLMG/AMReX_MLPoisson_3D_K.H
      ../Tests/Amr/PressureSolve/helpers.H
      ../Tests/Amr/PressureSolve/helpers.cpp
      ../Tests/Amr/PressureSolve/main.cpp
      * These files are going to all be changed to support internal fixed velocity BCs.
      ** But running clang-format on these files in combination with the functional changes
         obscures the functional changes. So just make the clang-format changes first.
    
    Walkthrough by DRB on 10/6/2025

--- Tests/Amr/PressureSolve/main.cpp
+++ Tests/Amr/PressureSolve/main.cpp
@@ -23,118 +23,98 @@ int MyMain();
 /*--------------------------------------------------------------------
   function definitions
   --------------------------------------------------------------------*/
-int main( int argc, char** argv )
-{
-  amrex::Initialize( argc, argv );
+int main(int argc, char **argv) {
+  amrex::Initialize(argc, argv);
   const int ret = MyMain();
   amrex::Finalize();
   return ret;
 }
 
-int MyMain()
-{
+int MyMain() {
   // Parse command line parameters
   int nlevels = 3;
   int baseN = 4;
   bool dense = true;
-  amrex::Real domain_length = 1.0;  // meters
+  amrex::Real domain_length = 1.0; // meters
   amrex::Real tolerance = 1.0e-8;
   int max_iterations = 500;
   // PressureSolve::StencilType stencil_type =
   //   PressureSolve::StencilType::Standard5Point;
   PressureSolve::StencilType stencil_type =
-    PressureSolve::StencilType::HOC9Point;
+      PressureSolve::StencilType::HOC9Point;
   {
     amrex::ParmParse pp;
     // std::string stencil_type_str = "standard5point";  /// DEBUG
-    std::string stencil_type_str = "hoc9point";  /// DEBUG
-    pp.query( "n_levels", nlevels );
-    pp.query( "base_n", baseN );
-    pp.query( "dense", dense );
-    pp.query( "domain_length", domain_length );
-    pp.query( "tolerance", tolerance );
-    pp.query( "max_iterations", max_iterations );
-    pp.query( "stencil_type", stencil_type_str );
+    std::string stencil_type_str = "hoc9point"; /// DEBUG
+    pp.query("n_levels", nlevels);
+    pp.query("base_n", baseN);
+    pp.query("dense", dense);
+    pp.query("domain_length", domain_length);
+    pp.query("tolerance", tolerance);
+    pp.query("max_iterations", max_iterations);
+    pp.query("stencil_type", stencil_type_str);
 
     // Convert stencil type string to enum
-    if ( stencil_type_str == "standard5point" ) {
+    if (stencil_type_str == "standard5point") {
       stencil_type = PressureSolve::StencilType::Standard5Point;
-    } else if ( stencil_type_str == "hoc9point" ) {
+    } else if (stencil_type_str == "hoc9point") {
       stencil_type = PressureSolve::StencilType::HOC9Point;
     } else {
       amrex::Abort(
-        "Unknown stencil type. Valid options are: standard5point, hoc9point" );
+          "Unknown stencil type. Valid options are: standard5point, hoc9point");
     }
 
-    amrex::Print()                                 //
-      << "Equivalent command line parameters:\n "  //
-      << " n_levels=" << nlevels                   //
-      << " base_n=" << baseN                       //
-      << " dense=" << dense                        //
-      << " domain_length=" << domain_length        //
-      << " tolerance=" << tolerance                //
-      << " max_iterations=" << max_iterations      //
-      << " stencil_type=" << stencil_type_str      //
-      << "\n";
+    amrex::Print()                                  //
+        << "Equivalent command line parameters:\n " //
+        << " n_levels=" << nlevels                  //
+        << " base_n=" << baseN                      //
+        << " dense=" << dense                       //
+        << " domain_length=" << domain_length       //
+        << " tolerance=" << tolerance               //
+        << " max_iterations=" << max_iterations     //
+        << " stencil_type=" << stencil_type_str     //
+        << "\n";
   }
 
   // Create composite mesh
-  std::vector<LevelData> composite_levels =  //
-    MakeSparseCompositeLevels(               //
-      baseN,
-      nlevels,
-      domain_length );
+  std::vector<LevelData> composite_levels = //
+      MakeSparseCompositeLevels(            //
+          baseN, nlevels, domain_length);
 
   // Solve on composite mesh
-  CompositeSolve(  //
-    composite_levels,
-    tolerance,
-    max_iterations,
-    baseN,
-    nlevels,
-    stencil_type );
+  CompositeSolve( //
+      composite_levels, tolerance, max_iterations, baseN, nlevels,
+      stencil_type);
 
-  for ( int lev = 0; lev < nlevels; ++lev ) {
-    CheckResults(  //
-      composite_levels[lev].pressure,
-      composite_levels[lev].geom,
-      baseN,
-      nlevels );
+  for (int lev = 0; lev < nlevels; ++lev) {
+    CheckResults( //
+        composite_levels[lev].pressure, composite_levels[lev].geom, baseN,
+        nlevels);
   }
 
-  if ( dense ) {
+  if (dense) {
     // First create and work with fine-level data
-    LevelData dense_level = MakeDenseLevelData(  //
-      domain_length,
-      CalculateFineN( baseN, nlevels ),
-      1,    // Only 1 level
-      0 );  // This is level zero
+    LevelData dense_level = MakeDenseLevelData( //
+        domain_length, CalculateFineN(baseN, nlevels),
+        1,  // Only 1 level
+        0); // This is level zero
 
     // Work with fine-level data
-    amrex::Print()  //
-      << "\nDense fine solution, domain: " << dense_level.geom.Domain()
-      << ", dx = " << dense_level.geom.CellSize()[0] << "\n";
-    SingleLevelPressureSolve(  //
-      dense_level.pressure,
-      dense_level.velocity,
-      dense_level.geom,
-      tolerance,
-      max_iterations,
-      baseN,
-      nlevels,
-      stencil_type );
+    amrex::Print() //
+        << "\nDense fine solution, domain: " << dense_level.geom.Domain()
+        << ", dx = " << dense_level.geom.CellSize()[0] << "\n";
+    SingleLevelPressureSolve( //
+        dense_level.pressure, dense_level.velocity, dense_level.geom, tolerance,
+        max_iterations, baseN, nlevels, stencil_type);
 
-    CheckResults( dense_level.pressure, dense_level.geom, baseN, nlevels );
+    CheckResults(dense_level.pressure, dense_level.geom, baseN, nlevels);
 
     // Compare dense results with sparse results
-    CompareMultiFabs( dense_level, composite_levels );
+    CompareMultiFabs(dense_level, composite_levels);
 
     // Sample results and create plots
-    SamplePressureAlongLine(  //
-      composite_levels,
-      dense_level,
-      baseN,
-      nlevels );
+    SamplePressureAlongLine( //
+        composite_levels, dense_level, baseN, nlevels);
   }
 
   return 0;
